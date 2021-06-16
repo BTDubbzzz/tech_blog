b9d6ca7e23b85fbfd01687b027288124e2ba688d
Blake <btdubbzzz@gmail.com>
2021-06-16 15:09:13 -0500

    added get post by id and corresponding view. added post route for new user post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,24 +26,41 @@ router.get('/', async (req, res) => {
 	}
 });
 
-router.get('/dashboard', withAuth, async (req, res) => {
+router.get('/post/:id', async (req, res) => {
 	try {
-		// Get all posts and JOIN with user data
-		const postData = await Post.findAll({
+		const postData = await Post.findByPk(req.params.id, {
 			include: [
 				{
 					model: User,
+					attributes: ['name'],
 				},
 			],
 		});
 
-		// Serialize data so the template can read it
-		const posts = postData.map((post) => post.get({ plain: true }));
+		const post = postData.get({ plain: true });
 
-		// Pass serialized data and session flag into template
-		res.render('homepage', {
-			posts,
+		res.render('post', {
+			...post,
 			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+	try {
+		// Find the logged in user based on the session ID
+		const userData = await User.findByPk(req.session.user_id, {
+			attributes: { exclude: ['password'] },
+			include: [{ model: Post }],
+		});
+
+		const user = userData.get({ plain: true });
+
+		res.render('dashboard', {
+			...user,
+			logged_in: true,
 		});
 	} catch (err) {
 		res.status(500).json(err);
