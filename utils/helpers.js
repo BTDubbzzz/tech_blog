af047068c8c39735d93636bc5705e072c4d90dec
Blake <btdubbzzz@gmail.com>
2021-06-16 23:36:09 -0500

    added date formatting to all dates with handlebars helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,8 @@
 const { User } = require('../models');
 
 module.exports = {
-	get_comment_username: async (id) => {
-		try {
-			const userData = await User.findByPk(id);
-			const user = userData.get({ plain: true });
-			console.log('user :>> ', user);
-			return user.name;
-		} catch (error) {
-			console.log('error :>> ', error);
-		}
+	format_date: (date) => {
+		// Format date as MM/DD/YYYY
+		return date.toLocaleDateString();
 	},
 };
